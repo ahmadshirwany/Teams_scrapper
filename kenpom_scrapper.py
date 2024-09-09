f23a0c71f4fdeff3046072a357f17c297b997f74
Ahmad Ali Khan Shirwany <shirwany.shirwany@gmail.com>
2024-09-10 00:18:37 +0300

    add requirements.txt file

--- kenpom_scrapper.py
+++ kenpom_scrapper.py
@@ -10,8 +10,16 @@ class HTMLTableExtractor:
 
     def fetch_page(self):
         # Fake headers to mimic a browser
+        # headers = {
+        #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+        # }
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'Referer': self.url,
         }
         response = requests.get(self.url, headers=headers)
         if response.status_code == 200:
