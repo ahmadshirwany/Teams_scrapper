699d986585c12d3f0d9b3d515fa949fd73f0ca90
Ahmad Ali Khan Shirwany <shirwany.shirwany@gmail.com>
2024-09-11 02:34:35 +0300

    update kenpom_scrapper.py

--- kenpom_scrapper.py
+++ kenpom_scrapper.py
@@ -13,8 +13,12 @@ class HTMLTableExtractor:
         # headers = {
         #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
         # }
+        from fake_useragent import UserAgent
+
+        ua = UserAgent()
+
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+            'User-Agent': ua.random,
             'Accept-Language': 'en-US,en;q=0.9',
             'Accept-Encoding': 'gzip, deflate, br',
             'Connection': 'keep-alive',
