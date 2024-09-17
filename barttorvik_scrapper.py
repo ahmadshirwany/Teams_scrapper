20f7593473cf8c15ae867d5765b5d2f8e9d87fbc
Ahmad Ali Khan Shirwany <shirwany.shirwany@gmail.com>
2024-09-17 15:51:28 +0300

    evanmiya_scrapper.py added

---
+++ barttorvik_scrapper.py
@@ -0,0 +1,65 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+class HTMLTableExtractor:
+    def __init__(self, url):
+        self.url = url
+        self.table_data = None
+        self.headers = None
+
+    def fetch_page(self):
+        # Fake headers to mimic a browser
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+        }
+
+        response = requests.get(self.url, headers=headers)
+        if response.status_code == 200:
+            return response.text
+        elif response.status_code == 403:
+            raise Exception("Access forbidden. The site may be blocking automated requests.")
+        else:
+            raise Exception(f"Failed to retrieve the page. Status code: {response.status_code}")
+
+
+    def parse_table(self):
+        html_content = self.fetch_page()
+        soup = BeautifulSoup(html_content, 'html.parser')
+        table = soup.find('table')  # You can modify this to locate the specific table
+        colums_tag =  table = soup.find('thead')
+        self.headers = [a.text for a in colums_tag.find_all('tr')[1].find_all('th')[0:]]
+        self.table_data = []
+        # Extract the headers
+        rows_tag = table = soup.find('tbody')
+        for row in rows_tag.find_all('tr')[0:]:
+            tags = row.find_all('td')
+            for tag in tags:
+                # Find all <span> elements within the <a> tag
+                spans = tag.find_all('span')
+
+                # Decompose each <span> (remove it from the tag)
+                for span in spans:
+                    span.decompose()
+            row_data =  [a.text for a in tags]
+            if row_data:
+                self.table_data.append(row_data)
+        print('data')
+
+
+
+    def to_dataframe(self):
+        if self.table_data and self.headers:
+            return pd.DataFrame(self.table_data, columns=self.headers)
+        else:
+            raise Exception("Table data has not been parsed yet. Call `parse_table` first.")
+
+    def save_to_csv(self, file_name):
+        df = self.to_dataframe()
+        df.to_csv(file_name, index=False)
+        print(f"Table saved to {file_name}")
+
+html_extractor = HTMLTableExtractor("https://www.barttorvik.com/#")
+html_extractor.parse_table()
+df = html_extractor.to_dataframe()
+html_extractor.save_to_csv("barttorvik_table.csv")
---
+++ evanmiya_scrapper.py
@@ -0,0 +1,79 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import pandas as pd
+from selenium.webdriver.support.ui import Select
+
+class HTMLTableExtractorSelenium:
+    def __init__(self, url):
+        self.url = url
+        self.table_data = None
+        self.headers = None
+
+    def fetch_page(self):
+        # Setup Chrome WebDriver using ChromeDriverManager
+        options = webdriver.ChromeOptions()
+        # options.add_argument("--headless")  # Run in headless mode
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-search-engine-choice-screen')
+        options.add_argument("--log-level=3")
+        options.add_argument("--silent")
+        options.add_argument("--disable-gpu")
+        options.add_argument(
+            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+
+        # Initialize the WebDriver
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=options)
+        driver.get(self.url)
+        time.sleep(5)
+        dropdown = driver.find_element(By.XPATH,
+                                       '//*[@id="team_ratings_page-team_ratings"]/div/div[2]/div[1]/div[2]/select')
+        select = Select(dropdown)
+        options = select.options
+        values = [int(option.text) for option in options if option.text.replace('.', '', 1).isdigit()]
+        max_value = max(values)
+        select.select_by_visible_text(str(max_value))
+        page_source = driver.page_source
+        return driver,page_source
+
+    def parse_table(self):
+        driver,html_content = self.fetch_page()
+        self.headers = driver.find_elements(By.CLASS_NAME,'rt-thead')[-1].text.split('\n')
+        driver.close()
+        soup = BeautifulSoup(html_content, 'html.parser')
+        row_elements = soup.find_all('div', class_='rt-tr rt-tr-striped-sticky rt-tr-highlight-sticky')
+        self.table_data = []
+        if row_elements:
+            for row in row_elements:
+                columns = row.find_all('div', class_='rt-td-inner')
+                column_values = [col.get_text(strip=True) for col in columns]
+                self.table_data.append(column_values)
+        else:
+            print("No elements found")
+
+
+
+
+    def to_dataframe(self):
+        if self.table_data and self.headers:
+            return pd.DataFrame(self.table_data, columns=self.headers)
+        else:
+            raise Exception("Table data has not been parsed yet. Call `parse_table` first.")
+
+    def save_to_csv(self, file_name):
+        df = self.to_dataframe()
+        df.to_csv(file_name, index=False)
+        print(f"Table saved to {file_name}")
+
+# Usage
+url = "https://www.evanmiya.com/?team_ratings"  # Change to the correct URL if needed
+html_extractor = HTMLTableExtractorSelenium(url)
+html_extractor.parse_table()
+df = html_extractor.to_dataframe()
+html_extractor.save_to_csv("evanmiya_table.csv")
